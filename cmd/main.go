33dc9c31a1666aea2fa41a0c8e7dc65900305576
danilBogo <86552329+danilBogo@users.noreply.github.com>
2024-01-26 19:19:58 +0300

    add realization

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
+const binancePriceUrl = "https://api.binance.com/api/v3/ticker/price"
+
+type Data struct {
+	Symbol string `json:"symbol"`
+	Price  string `json:"price"`
+}
+
+func main() {
+	response, err := http.Get(binancePriceUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	bytesBuffer := bytes.Buffer{}
+	buffer := make([]byte, 1024)
+	for {
+		bytesRead, err := response.Body.Read(buffer)
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+
+		bytesBuffer.Write(buffer[:bytesRead])
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	var arrData []Data
+	err = json.Unmarshal(bytesBuffer.Bytes(), &arrData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(arrData, len(arrData))
 }
